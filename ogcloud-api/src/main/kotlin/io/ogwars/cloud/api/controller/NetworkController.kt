fc9801e25b1fa62f3d152420d59ce279c07a67a7
Jevzo <jerrygit@outlook.com>
2026-03-17 04:43:49 +0100

    Add network restart api endpoint

--- ogcloud-api/src/main/kotlin/io/ogwars/cloud/api/controller/NetworkController.kt
+++ ogcloud-api/src/main/kotlin/io/ogwars/cloud/api/controller/NetworkController.kt
@@ -4,18 +4,23 @@ import io.ogwars.cloud.api.dto.MaintenanceToggleRequest
 import io.ogwars.cloud.api.dto.NetworkSettingsResponse
 import io.ogwars.cloud.api.dto.NetworkStatusResponse
 import io.ogwars.cloud.api.dto.UpdateNetworkRequest
+import io.ogwars.cloud.api.service.NetworkRestartService
 import io.ogwars.cloud.api.service.NetworkService
 import jakarta.validation.Valid
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/v1/network")
 class NetworkController(
     private val networkService: NetworkService,
+    private val networkRestartService: NetworkRestartService,
 ) {
     @GetMapping
     fun getSettings(): NetworkSettingsResponse = networkService.getSettings()
@@ -32,4 +37,8 @@ class NetworkController(
     fun toggleMaintenance(
         @RequestBody request: MaintenanceToggleRequest,
     ): NetworkSettingsResponse = networkService.setMaintenance(request.maintenance)
+
+    @PostMapping("/restart")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    fun restartNetwork() = networkRestartService.requestRestart()
 }
